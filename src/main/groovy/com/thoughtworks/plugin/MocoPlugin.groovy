96b3242841cc7a67d8fea7749974cd103295d528
dwyin <silverjava@gmail.com>
2013-04-07 14:18:33 +0800

    add gradle wrapper

--- src/main/groovy/com/thoughtworks/plugin/MocoPlugin.groovy
+++ src/main/groovy/com/thoughtworks/plugin/MocoPlugin.groovy
@@ -4,15 +4,11 @@ import com.thoughtworks.plugin.tasks.StartMocoTask
 import com.thoughtworks.plugin.tasks.StopMocoTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.Configuration
 
 class MocoPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        Configuration config = project.configurations.add('moco')
-        config.dependencies.add('com.github.dreamhead:moco-runner:0.6.3-SNAPSHOT')
-
         project.task('startMoco', type: StartMocoTask)
         project.task('stopMoco', type: StopMocoTask)
     }
